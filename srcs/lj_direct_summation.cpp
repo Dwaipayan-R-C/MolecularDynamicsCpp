bbc52e8b082e737f68217189801e6cf4eda1a4eb
Dwaipayan-R-C <dwaipayan0502@gmail.com>
2022-07-30 14:12:05 +0200

    oriented folder structure

---
+++ srcs/lj_direct_summation.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Dwaipayan on 20-05-2022.
+//
+#include <iostream>
+#include "../header/lj_direct_summation.h"
+#include "../header/neighbors.h"
+
+
+Array3Xd lj_direct_summation_force(Atoms &atoms){
+    int i, j, k;
+    double f, rSqd;
+    double rij[3];
+    Eigen::Vector3d distance_vector;
+    Array3Xd accln(3,nb_atoms);
+    accln = 0;
+    Array3Xd pos = atoms.positions;
+    NeighborList neighbor_list(rc);
+    neighbor_list.update(atoms);
+    for (auto[i, j]: neighbor_list) {
+        if (i < j) {
+            rSqd = 0;
+            distance_vector = (pos.col(i) - pos.col(j));
+            rSqd = pow(distance_vector.norm(),2);
+            f = 24*eps*(2*pow(sigma,12)*pow(rSqd, -7) - pow(sigma,6)*pow(rSqd, -4));
+            accln.col(i) += (pos.col(i) - pos.col(j))*f/mass;
+            accln.col(j) -= (pos.col(i) - pos.col(j))*f/mass;
+        }
+    }
+    return accln;
+}
+//
+// Created by Dwaipayan on 20-05-2022.
+//
+
+//#include "header/lj_direct_summation.h"
+//
+//Array3Xd lj_direct_summation_force(Array3Xd &pos){
+//    int i, j, k;
+//    double f, rSqd;
+//    double rij[3];
+//    Eigen::Vector3d distance_vector;
+//    Array3Xd accln(3,nb_atoms);
+//    accln = 0;
+//    for (i = 0; i < nb_atoms-1; i++) {   // loop over all distinct pairs i,j
+//        for (j = i+1; j < nb_atoms; j++) {
+//            // initialize r^2 to zero
+//            rSqd = 0;
+//            distance_vector = (pos.col(i) - pos.col(j));
+//            rSqd = pow(distance_vector.norm(),2);
+//            f = 24*eps*(2*pow(sigma,12)*pow(rSqd, -7) - pow(sigma,6)*pow(rSqd, -4));
+////            f = 48*eps*(pow(sigma/distance_vector.norm(),6)*(2*pow(sigma/distance_vector.norm(),6)-0.5))/rSqd;
+//            accln.col(i) += (pos.col(i) - pos.col(j))*f*mass;
+//            accln.col(j) -= (pos.col(i) - pos.col(j))*f*mass;
+//
+//        }
+//    }
+//    return accln;
+//}
---
+++ srcs/verlet.cpp
@@ -0,0 +1,59 @@
+
+#include "Eigen/Core"
+#include "../header/verlet.h"
+#include "../header/berendsen_thermostat.h"
+
+
+using namespace Eigen;
+
+void initialize(Atoms &atoms) {
+    int n, p, i, j, k;
+    double pos;
+
+    // Number of atoms in each direction
+    n = int(ceil(pow(nb_atoms, 1.0/3)));
+
+    //  spacing between atoms along a given direction
+    pos = 20 / n;
+
+    //  index for number of particles assigned positions
+    p = 0;
+    //  initialize positions
+    for (i=0; i<n; i++) {
+        for (j=0; j<n; j++) {
+            for (k=0; k<n; k++) {
+                if (p<nb_atoms) {
+
+                    atoms.positions(0,p) = (i + 0.5)*pos;
+                    atoms.positions(1,p) = (j + 0.5)*pos;
+                    atoms.positions(2,p) = (k + 0.5)*pos;
+                }
+                p++;
+            }
+        }
+    }
+}
+void Verlet_one(double dt, Atoms &atoms) {
+    int i;
+    //  Update positions and velocity with current velocity and acceleration
+
+    for (i = 0; i < atoms.nb_atoms(); i++) {
+        atoms.velocities.col(i) += 0.5 * atoms.forces.col(i) * dt;
+        atoms.positions.col(i) += atoms.velocities.col(i) * dt + 0.5*atoms.forces.col(i)*dt*dt;
+    }
+//    std::cout << atoms.velocities.sum()<< std::endl;
+
+}
+
+
+void Verlet_two(double dt, Atoms &atoms) {
+    int i;
+    //  Update velocity with updated acceleration
+    for (i = 0; i < atoms.nb_atoms(); i++) {
+        atoms.velocities.col(i) += 0.5 * atoms.forces.col(i) * dt;
+    }
+//    std::cout << atoms.velocities.sum()<< std::endl;
+//    atoms.velocities = berendsen_thermostat(atoms,10, timescale, 10*timescale);
+
+}
+
